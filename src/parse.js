5c6b6ec8c39e305680bf26f36bd518a29e607822
Kevin Morehouse <khmorehouse@gmail.com>
2019-05-13 08:49:01 -0400

    Add integer parsing, with test

--- src/parse.js
+++ src/parse.js
@@ -8,16 +8,62 @@ function Lexer() {
 }
 
 Lexer.prototype.lex = function(text) {
-  // Tokenization will be done here
+  this.text = text;
+  this.index = 0;
+  this.ch = undefined;
+  this.tokens = [];
+
+  while (this.index < this.text.length) {
+    this.ch = this.text.charAt(this.index);
+    if (this.isNumber(this.ch)) {
+      this.readNumber();
+    } else {
+      throw 'Unexpected next character: ' + this.ch;
+    }
+  }
+
+  return this.tokens;
+};
+
+Lexer.prototype.isNumber = function(ch) {
+  return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function() {
+  var number = '';
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (this.isNumber(ch)) {
+      number += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+  this.tokens.push({
+    text: number,
+    value: Number(number)
+  });
 };
 
 function AST(lexer) {
   this.lexer = lexer;
 }
 
+AST.Program = 'Program';
+AST.Literal = 'Literal';
+
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
-  // AST building will be done here
+  return this.program();
+};
+
+AST.prototype.program = function() {
+  return {type: AST.Program, body: this.constant()};
+};
+
+AST.prototype.constant = function() {
+  return {type: AST.Literal, value: this.tokens[0].value};
 };
 
 function ASTCompiler(astBuilder) {
@@ -26,7 +72,21 @@ function ASTCompiler(astBuilder) {
 
 ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
-  // AST compilation will be done here
+  this.state = {body: []};
+  this.recurse(ast);
+  /* jshint -W054 */
+  return new Function(this.state.body.join(''));
+  /* jshint +W054 */
+};
+
+ASTCompiler.prototype.recurse = function(ast) {
+  switch(ast.type) {
+    case AST.Program:
+      this.state.body.push('return ', this.recurse(ast.body), ';');
+      break;
+    case AST.Literal:
+      return ast.value;
+  }
 };
 
 function Parser(lexer) {
