b0a0c69b95fde7b7746701b53bfddb14c4454747
Kevin Morehouse <khmorehouse@gmail.com>
2019-07-27 22:03:35 -0400

    Add negated filtering with strings, with test

--- test/filter_filter_spec.js
+++ test/filter_filter_spec.js
@@ -99,5 +99,10 @@ describe('filter filter', function() {
     expect(fn({arr: [undefined, 'undefined']})).toEqual(['undefined']);
   });
 
+  it('allows negating string filter', function() {
+    var fn = parse('arr | filter:"!o"');
+    expect(fn({arr: ['quick', 'brown', 'fox']})).toEqual(['quick']);
+  });
+
 
 });
