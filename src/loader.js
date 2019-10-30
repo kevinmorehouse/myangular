2b3bc0f86601d9996bfcb3c59bc27e22cf51a25f
Kevin Morehouse <khmorehouse@gmail.com>
2019-10-30 10:39:34 -0400

    Add one-time initialization of the global and the module method, with tests

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function setupModuleLoader(window) {
-  var angular = window.angular = {};
+  var ensure = function(obj, name, factory) {
+    return obj[name] || (obj[name] = factory());
+  };
+
+  var angular = ensure(window, 'angular', Object);
+
+  ensure(angular, 'module', function() {
+    return function() {
+
+    };
+  });
 }
 
 module.exports = setupModuleLoader;
--- test/loader_spec.js
+++ test/loader_spec.js
@@ -4,9 +4,32 @@ var setupModuleLoader = require('../src/loader');
 
 describe('setupModuleLoader', function() {
 
+  beforeEach(function() {
+    delete window.angular;
+  });
+
   it('exposes angular on the window', function() {
     setupModuleLoader(window);
     expect(window.angular).toBeDefined();
+  });
+
+  it('creates angular just once', function() {
+    setupModuleLoader(window);
+    var ng = window.angular;
+    setupModuleLoader(window);
+    expect(window.angular).toBe(ng);
+  });
+
+  it('exposes the angular module function', function() {
+    setupModuleLoader(window);
+    expect(window.angular.module).toBeDefined();
+  });
+
+  it('exposes the angular module function just once', function() {
+    setupModuleLoader(window);
+    var module = window.angular.module;
+    setupModuleLoader(window);
+    expect(window.angular.module).toBe(module);
   });
 
 });
