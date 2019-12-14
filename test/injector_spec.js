6b7f06473e769025a7628503593cc14f04b6849b
Kevin Morehouse <khmorehouse@gmail.com>
2019-12-14 10:21:11 -0500

    Add binding of this in injected functions, with test

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -111,4 +111,18 @@ describe('injector', function() {
     }).toThrow();
   });
 
+  it('invokes a function with a given this context', function() {
+    var module = window.angular.module('myModule', []);
+    module.constant('a', 1);
+    var injector = createInjector(['myModule']);
+
+    var obj = {
+      two: 2,
+      fn: function(one) { return one + this.two; }
+    };
+    obj.fn.$inject = ['a'];
+
+    expect(injector.invoke(obj.fn, obj)).toBe(3);
+  });
+
 });
