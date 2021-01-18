2216698c65623f6736866002b7aa3458dad52edb
Kevin Morehouse <khmorehouse@gmail.com>
2021-01-18 09:49:25 -0500

    Add rejection of Deferreds and rejection catching

--- src/q.js
+++ src/q.js
@@ -9,12 +9,16 @@ function $QProvider() {
     function Promise() {
       this.$$state = {};
     }
-    Promise.prototype.then = function(onFulfilled) {
+    Promise.prototype.then = function(onFulfilled, onRejected) {
       this.$$state.pending = this.$$state.pending || [];
-      this.$$state.pending.push(onFulfilled);
+      this.$$state.pending.push([null, onFulfilled, onRejected]);
       if (this.$$state.status > 0) {
         scheduleProcessQueue(this.$$state);
       }
+    };
+
+    Promise.prototype.catch = function(onRejected) {
+      return this.then(null, onRejected);
     };
 
     function Deferred() {
@@ -29,6 +33,18 @@ function $QProvider() {
       scheduleProcessQueue(this.promise.$$state);
     };
 
+    Deferred.prototype.reject = function(reason) {
+      if (this.promise.$$state.status) {
+        return;
+      }
+      this.promise.$$state.value = reason;
+      this.promise.$$state.status = 2;
+      scheduleProcessQueue(this.promise.$$state);
+    };
+
+
+
+
     function defer() {
       return new Deferred();
     }
@@ -42,8 +58,11 @@ function $QProvider() {
     function processQueue(state) {
       var pending = state.pending;
       state.pending = undefined;
-      _.forEach(pending, function(onFulfilled) {
-        onFulfilled(state.value);
+      _.forEach(pending, function(handlers) {
+        var fn = handlers[state.status];
+        if (_.isFunction(fn)) {
+          fn(state.value);
+        }
       });
     }
 
